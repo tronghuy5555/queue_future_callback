1e59d3adcf84c4e7bb3f46b35a9c6ce2a740ea2d
Huy Pham <tronghuy5555@gmail.com>
2023-11-05 16:14:23 +0700

    init package

--- lib/src/queue_future_callback.dart
+++ lib/src/queue_future_callback.dart
@@ -4,32 +4,45 @@ import 'package:flutter/rendering.dart';
 
 enum QueueCallbackStatus {
   initial,
-  showing,
+  consuming,
   pause,
-  done,
+  resume,
+  consumeSuccess,
+  disposed,
 }
 
 Duration kDelayTime = const Duration(milliseconds: 100);
 
 class QueueFutureCallback {
   static const String TAG = 'QueueFutureCallback';
-  Duration? delayTime;
+  final Duration? delayTime;
+  final ValueChanged<QueueCallbackStatus>? onStatusChanged;
 
   QueueFutureCallback({
     this.delayTime,
+    this.onStatusChanged,
   });
 
   final Queue<Future? Function()> futureCallbacks = Queue();
   QueueCallbackStatus _status = QueueCallbackStatus.initial;
-  bool get isShowingPopUp => _status == QueueCallbackStatus.showing;
+  QueueCallbackStatus get status => _status;
 
-  void addFutureIntoQueue(Future? Function() futureCallBack) async {
+  bool get isConsumingCallback => _status == QueueCallbackStatus.consuming;
+
+  bool shouldConsumeCallBack() {
+    return _status == QueueCallbackStatus.consumeSuccess ||
+        _status == QueueCallbackStatus.initial ||
+        _status == QueueCallbackStatus.resume;
+  }
+
+  Future? addFutureIntoQueue(Future? Function() futureCallBack) async {
     try {
-      if (_status == QueueCallbackStatus.done ||
-          _status == QueueCallbackStatus.initial) {
+      if (shouldConsumeCallBack()) {
         futureCallbacks.add(futureCallBack);
+        debugPrint(
+            '[$TAG] - add future callback success ${futureCallBack.hashCode}');
         await _consumeCallbackWhenReady();
-      } else {
+      } else if (status != QueueCallbackStatus.disposed) {
         futureCallbacks.add(futureCallBack);
       }
     } catch (e, _) {
@@ -41,32 +54,54 @@ class QueueFutureCallback {
   }
 
   Future? _consumeCallbackWhenReady() async {
-    _status = QueueCallbackStatus.showing;
+    if (!shouldConsumeCallBack()) {
+      return;
+    }
+    _onStatusChanged(QueueCallbackStatus.consuming);
     while (futureCallbacks.isNotEmpty) {
       final latestCallback = futureCallbacks.removeFirst();
       await Future.delayed(delayTime ?? kDelayTime);
       if (_status == QueueCallbackStatus.pause) {
         //*Put latest callback in the first position and exit the loop
+        debugPrint(
+            '[$TAG] - Consuming - pause future callback and put latest callback into first position ${latestCallback.hashCode}');
         futureCallbacks.addFirst(latestCallback);
         return;
       }
+      if (_status == QueueCallbackStatus.disposed) {
+        debugPrint('[$TAG] - Consuming - disposed future callback');
+        return;
+      }
+      debugPrint(
+          '[$TAG]- Consuming - consume future callback success ${latestCallback.hashCode}');
       await latestCallback.call();
       await Future.delayed(delayTime ?? kDelayTime);
     }
-    _status = QueueCallbackStatus.done;
+    _onStatusChanged(QueueCallbackStatus.consumeSuccess);
   }
 
   void pauseConsumingCallback() {
-    _status = QueueCallbackStatus.pause;
+    _onStatusChanged(QueueCallbackStatus.pause);
   }
 
-  void resumeConsumingCallback() {
+  Future resumeConsumingCallback() async {
     if (_status == QueueCallbackStatus.pause) {
-      _consumeCallbackWhenReady();
+      _onStatusChanged(QueueCallbackStatus.resume);
+      await _consumeCallbackWhenReady();
+    }
+  }
+
+  void _onStatusChanged(QueueCallbackStatus status) {
+    if (_status != status) {
+      debugPrint('[$TAG] - status changed: $_status ----> $status');
+      _status = status;
+      onStatusChanged?.call(status);
     }
   }
 
   void dispose() {
+    debugPrint('[$TAG] - disposed queue');
+    _onStatusChanged(QueueCallbackStatus.disposed);
     futureCallbacks.clear();
   }
 }
